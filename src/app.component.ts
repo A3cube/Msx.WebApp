b5a633c46569d6014c0641ff66af7af6933035ba
marcello-brocco-pi <marcello.brocco@acube.ai>
2025-10-02 16:11:53 +0200

    Start Template step 5

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { ConfirmDialog } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterModule],
-    template: `<router-outlet></router-outlet>`
+    imports: [RouterModule, ConfirmDialog],
+    providers: [ConfirmationService],
+    template: `
+        <p-confirmdialog [style]="{ width: '50vw' }"/>
+        <router-outlet></router-outlet>`
 })
-export class AppComponent {}
+export class AppComponent {
+    constructor(private router : Router) {
+        const currentUrl = window.location.pathname;
+        if (currentUrl !== '/') {
+            this.router.navigate(['/']);
+        }
+    }
+}
